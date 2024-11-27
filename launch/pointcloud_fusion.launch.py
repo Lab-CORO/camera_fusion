f8ad700465e7a540fe7ca0fe4ab30d11f1cc3495
KarynaFL <karyna-f.ladouceur.1@ens.etsmtl.ca>
2024-11-27 15:37:13 -0500

    correction of the launch to have a correct tree

--- launch/pointcloud_fusion.launch.py
+++ launch/pointcloud_fusion.launch.py
@@ -7,15 +7,15 @@ def generate_launch_description():
         package='tf2_ros',
         executable='static_transform_publisher',
         name='static_transform_publisher_cam1',
-        arguments=['-0.0171', '-0.0858', '0.0606', '-0.0287', '-0.0315', '-2.8281', 'camera_link', 'link_6'],
+        arguments=['-0.0171', '-0.0858', '0.0606', '-0.0287', '-0.0315', '-2.8281', 'link_6', 'camera_link'],
         output='log'
     )
      # Static transform publisher for kinet result from calibration
     static_transform_publisher_2 = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        name='static_transform_publisher_cam1',
-        arguments=['-0.1962', '-0.2486', '0.8843', '-1.4077', '-0.1897', '-0.7588', 'camera_base', 'base_link'],
+        name='static_transform_publisher_cam2',
+        arguments=['-0.1962', '-0.2486', '0.8843', '-1.4077', '-0.1897', '-0.7588', 'base_link', 'camera_base'],
         output='log'
     )
 
